eefbe7532df127d6020612c3a297f9a3053d215f
Mich-Tapawan <mik.tapawan@gmail.com>
2026-06-17 12:22:51 +0800

    feat(footer): Add Social Media Link Icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,37 @@
+import SocialMediaIcons from "./SocialMediaIcons";
+
 export default function Footer() {
   return (
-    <footer className="bg-primary text-white w-full min-h-100 px-4 py-12 lg:px-12 xl:px-20 2xl:px-32">
+    <footer className="bg-primary text-white text-sm w-full min-h-100 px-4 py-12 lg:px-12 xl:px-20 2xl:px-32">
       <div className="grid place-content-center text-center gap-10 xl:grid-cols-4 xl:text-start">
-        <div>
+        <div className="space-y-8">
           <p className="text-white font-bold" style={{ fontSize: "1.2rem" }}>
             Matcha Time
           </p>
+          <SocialMediaIcons />
         </div>
 
         <div className="flex flex-col gap-10">
-          <ul>
+          <ul className="space-y-2">
             <li className="font-medium">Get in touch</li>
-            <li className="">matchatime@gmail.com</li>
-            <li className="">555-0199</li>
-            <li className="">+1 212-555-0143</li>
+            <li className="font-extralight">matchatime@gmail.com</li>
+            <li className="font-extralight">555-0199</li>
+            <li className="font-extralight">+1 212-555-0143</li>
           </ul>
 
-          <ul>
+          <ul className="space-y-2">
             <li className="font-medium">Company</li>
-            <li className="">Terms & Conditions</li>
-            <li className="">Privacy Policy</li>
+            <li className="font-extralight">Terms & Conditions</li>
+            <li className="font-extralight">Privacy Policy</li>
           </ul>
         </div>
 
-        <ul>
+        <ul className="space-y-2">
           <li className="font-medium">Pages</li>
-          <li className="">Home</li>
-          <li className="">Menu</li>
-          <li className="">About</li>
-          <li className="">Contact</li>
+          <li className="font-extralight">Home</li>
+          <li className="font-extralight">Menu</li>
+          <li className="font-extralight">About</li>
+          <li className="font-extralight">Contact</li>
         </ul>
 
         <div className="flex flex-col space-y-3 items-center xl:items-start">
--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -1,13 +1,37 @@
 import InstagramIcon from "@iconify-react/mdi/instagram";
 import FacebookIcon from "@iconify-react/mdi/facebook";
 import TwitterIcon from "@iconify-react/mdi/twitter";
+import YoutubeIcon from "@iconify-react/mdi/youtube";
+import { Link } from "react-router-dom";
+
+type SocialIconType = {
+  component: React.ComponentType<any>;
+  link: string;
+};
+
+const SocialIcons: SocialIconType[] = [
+  { component: InstagramIcon, link: "https://www.instagram.com/" },
+  { component: FacebookIcon, link: "https://www.facebook.com/" },
+  { component: TwitterIcon, link: "https://www.twitter.com/" },
+  { component: YoutubeIcon, link: "https://www.youtube.com/" },
+];
 
 export default function SocialMediaIcons() {
   return (
-    <div className="flex items-center gap-2">
-      <InstagramIcon height="1em" style={{ color: "#fffdee" }} />
-      <FacebookIcon height="1em" style={{ color: "#fffdee" }} />
-      <TwitterIcon height="1em" style={{ color: "#fffdee" }} />
-    </div>
+    <ul className="flex items-center gap-2">
+      {SocialIcons.map(({ component: IconComponent, link }, index) => (
+        <li key={index}>
+          <Link to={link}>
+            {
+              <IconComponent
+                height="3em"
+                style={{ color: "#fffdee" }}
+                className="cursor-pointer"
+              />
+            }
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 }
